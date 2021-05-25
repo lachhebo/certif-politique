deca5d4d334f18077b1a84664a1f9a6720d9081c
Ismaïl Lachheb <ismael.lachheb@protonmail.com>
2021-05-25 12:46:52 +0200

    feat: read database

---
+++ certifia/data_engineering/data_access.py
@@ -0,0 +1,20 @@
+from typing import Dict
+
+import pandas as pd
+
+from certifia.data_engineering.database import Batch1DB, Batch2DB, TestDB
+
+
+def read_db() -> Dict:
+    datasets = {}
+    for base in [Batch1DB(), Batch2DB(), TestDB()]:
+        datasets[base.name] = {}
+        for table in base.tables:
+            with base:
+                df = pd.read_sql_query(f'SELECT * from {table}', base.conn)
+                datasets[base.name][table] = df
+
+    return datasets
+
+
+read_db()
--- tests/test_database.py
+++ tests/test_database.py
@@ -8,7 +8,7 @@ TESTED_MODULE = 'certifia.database'
 def test_session__is_connecting_to_the_good_database(path, monkeypatch):
     # given
     monkeypatch.setenv('DEBUG', 'False')
-    from certifia.database import Session
+    from certifia.data_engineering.database import Session
 
     # when
     sess = Session()
@@ -21,7 +21,7 @@ def test_session__is_connecting_to_the_good_database(path, monkeypatch):
 def test_get_user_password_by_email__return_user_password_if_user_found(m_sqlite, monkeypatch):
     # given
     monkeypatch.setenv('DEBUG', 'False')
-    from certifia.database import Session
+    from certifia.data_engineering.database import Session
     sess = Session()
     m_sqlite.connect().execute().fetchall.return_value = [('fake_password1',)]
 
@@ -35,7 +35,7 @@ def test_get_user_password_by_email__return_user_password_if_user_found(m_sqlite
 @patch(f'{TESTED_MODULE}.sqlite3')
 def test_get_user_password_by_email__return_none_if_user_not_found(m_sqlite, monkeypatch):
     # given
-    from certifia.database import Session
+    from certifia.data_engineering.database import Session
     sess = Session()
     m_sqlite.connect().execute().fetchall.return_value = []
 
