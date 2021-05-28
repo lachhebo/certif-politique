a3f1ea35acc6355375ea0d67b80359b7fa1d475b
Fabien Roussel <fabien.roussel@accenture.com>
2021-05-29 00:45:58 +0200

    feat: refacto into Unittest test case class

--- tests/utils/test_logger.py
+++ tests/utils/test_logger.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from unittest import TestCase
 from unittest.mock import patch
 
 import pytest
@@ -9,30 +10,31 @@ from certifia.utils.logger import Logger
 TESTED_MODULE = 'certifia.utils.logger'
 
 
-@freeze_time("2012-01-14")
-@patch(f'{TESTED_MODULE}.logging.FileHandler')
-def test_set_config__save_logs_in_a_specific_file(mock_file_handler):
-    # given
-    logger = Logger()
+class TestLogger(TestCase):
+    @freeze_time("2012-01-14")
+    @patch(f'{TESTED_MODULE}.logging.FileHandler')
+    def test_set_config__save_logs_in_a_specific_file(self, mock_file_handler):
+        # given
+        logger = Logger()
 
-    # when
-    logger.set_config(Path('/root'))
-
-    # then
-    mock_file_handler.assert_called_with('/root/logs/2012-01-14.log')
-
-
-@pytest.mark.parametrize('method_name', ['debug', 'info', 'critical', 'warning'])
-@patch(f'{TESTED_MODULE}.logging.FileHandler')
-def test_set_config__use_good_logging_method(mock_file_handler, method_name):
-    # given
-    logger = Logger()
-
-    # when
-    with patch(f'{TESTED_MODULE}.logging.Logger.{method_name}'):
-        method = getattr(logger, method_name)
-        method('fake message')
+        # when
+        logger.set_config(Path('/root'))
 
         # then
-        mocked_method = getattr(logger.logger, method_name)
-        mocked_method.assert_called_once_with('fake message')
+        mock_file_handler.assert_called_with('/root/logs/2012-01-14.log')
+
+
+    @pytest.mark.parametrize('method_name', ['debug', 'info', 'critical', 'warning'])
+    @patch(f'{TESTED_MODULE}.logging.FileHandler')
+    def test_set_config__use_good_logging_method(self, mock_file_handler, method_name):
+        # given
+        logger = Logger()
+
+        # when
+        with patch(f'{TESTED_MODULE}.logging.Logger.{method_name}'):
+            method = getattr(logger, method_name)
+            method('fake message')
+
+            # then
+            mocked_method = getattr(logger.logger, method_name)
+            mocked_method.assert_called_once_with('fake message')
