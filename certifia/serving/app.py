2fd1b519cd01434fd37000bab4ff6af42f1848ea
Fabien Roussel <fabien.roussel@accenture.com>
2021-05-31 15:58:34 +0200

    feat: load csv and make random prediction

--- certifia/serving/app.py
+++ certifia/serving/app.py
@@ -1,4 +1,7 @@
+from random import randrange, seed
+
 from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -19,12 +22,28 @@ def data():
     if request.method == 'GET':
         return "The URL /data is accessed directly. Try going to '/' to submit form"
     if request.method == 'POST':
-        print(request)
-        form_data = request.form
-        dummy_prediction(request.form)
+        df = load_csv(request.files.get('file'))
+        df_prediction = prediction(df)
 
-        return render_template('data.html', form=form_data)
+        return render_template('data.html', tables=[df_prediction.to_html(classes='data', header="true")])
 
 
-def dummy_prediction():
-    return {'retard': 4}
+def load_csv(file):
+    return pd.read_csv(file)
+
+
+def prediction(df):
+    # Load feature Engineering
+    # feature_engineering = FeatureEngineering()
+    # df_engineered = feature_engineering.transform(df)
+    # load training
+    # training = Training()
+    # y_pred = training.predict(df_engineered)
+    # concat y_pred and flight number
+    # df_prediction = concat([df[['IDENTIFIANT']], pd.Series(data=y_pred, name="PREDICTION"])
+
+    # temporairement
+    seed(42)
+    df_prediction = df[['IDENTIFIANT']].copy()
+    df_prediction.loc[:, 'PREDICTION'] = df['IDENTIFIANT'].apply(lambda x: randrange(10))
+    return df_prediction
