677db8eea3b4f27f7720013d54bd9fea96bb6b20
Fabien Roussel <fabien.roussel@accenture.com>
2021-06-08 17:38:06 +0200

    feat explor data and improve modele slightly

--- app/data_engineering/data_cleaning.py
+++ app/data_engineering/data_cleaning.py
@@ -7,11 +7,23 @@ class DataCleaning:
     def __init__(self, features_columns, label):
         self.features_columns = features_columns
         self.label = label
+        self.cie_by_avion = None
 
     def cleaning(self, df):
         df = df.dropna(subset=self.features_columns)
         if self.label in df.columns:
             df = df.dropna(subset=[self.label])
+        return df
+
+    def fill_na(self, df):
+        if self.cie_by_avion is None:
+            self.cie_by_avion = df[['CODE AVION', 'COMPAGNIE AERIENNE']].dropna().groupby(
+                by=['CODE AVION']).first().to_dict()['COMPAGNIE AERIENNE']
+
+        df.loc[df['COMPAGNIE AERIENNE'].isna(), 'COMPAGNIE AERIENNE'] = df.loc[
+            df['COMPAGNIE AERIENNE'].isna(), 'CODE AVION'].apply(
+                lambda x: self.cie_by_avion.get(x, "UKN")
+            )
         return df
 
     def transform(self, df):
